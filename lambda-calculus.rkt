bc30e9b33af15683b8c79d013adad2acfd10c53e
oomaschloom <noway@goaway.com>
2022-12-12 11:32:28 +1000

    Identity applied to identity returns identity - it's all we have

--- lambda-calculus.rkt
+++ lambda-calculus.rkt
@@ -1,6 +1,6 @@
 #lang pl schlac
 
-;;; I'm using the language schlac, a racket #lang, create by Eli Barzilay.
+;;; I'm using the language schlac, a racket #lang, created by Eli Barzilay.
 ;;; It models the base lambda calculus and details can be found at
 ;;; https://pl.barzilay.org/lec13#lambda-calculus-----schlac
 
@@ -10,5 +10,4 @@
 ;;; The identity function returns the argument to which it is applied
 (define identity (λ (x) x))
 
-(test (identity 'x) => 'x)
-(test (identity 'y) => 'y)
+(test (identity identity) => identity)
