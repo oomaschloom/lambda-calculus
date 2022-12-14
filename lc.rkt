77b70dafb547a9016d3b53fa6943c559649c9e4a
oomaschloom <noway@goaway.com>
2022-12-14 17:06:22 +1000

    second chapter - just the language

---
+++ lc.rkt
@@ -0,0 +1,15 @@
+#lang pl schlac
+
+(define identity (λ(x) x))
+(define self-apply (λ(s) (s s)))
+(define apply (λ(func) (λ(arg) (func arg))))
+
+(define make-pair (λ(first) (λ(second) (λ(func) ((func first) second)))))
+(define select-first (λ(first) (λ(second) first)))
+(define select-second (λ(first) (λ(second) second)))
+
+(define make-triplet
+  (λ(first) (λ(second) (λ(third) (λ(func) (((func first) second) third))))))
+(define triplet-first (λ(first) (λ(second) (λ(third) first))))
+(define triplet-second (λ(first) (λ(second) (λ(third) second))))
+(define triplet-third (λ(first) (λ(second) (λ(third) third))))
